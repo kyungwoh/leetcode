41f27f17868d341d9424d81caaa0d2c50318d163
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-07-06 11:59:35 -0700

    Update container-with-most-water.java

--- container-with-most-water.java
+++ container-with-most-water.java
@@ -1,4 +1,6 @@
+// https://leetcode.com/problems/container-with-most-water/description/
 class Solution {
+    // Time: O(n), Space: O(1)
     public int maxArea(int[] height) {
         int l = height.length;
         if(l==0) return 0;
@@ -10,4 +12,31 @@ class Solution {
         }
         return maxArea;
     }
+    
+    // Time: O(nlogn), Space: 0(n)
+    public int maxArea2(int[] height) {
+        int l = height.length;
+        if(l==0) return 0;
+        Point[] pList = new Point[l];
+        for(int i=0; i<l; i++) pList[i] = new Point(height[i], i);
+        Arrays.sort(pList);
+        //System.out.println(Arrays.toString(pList));
+        int left=l, right=-1, maxArea=0;
+        for(Point p : pList){
+            left = Math.min(left, p.index);
+            right = Math.max(right, p.index);
+            maxArea = Math.max(maxArea, p.height * (right-left));
+        }
+        return maxArea;
+    }
+}
+
+class Point implements Comparable<Point>{
+    int height, index;
+    Point(int height, int index){
+        this.height = height;
+        this.index = index;
+    }
+    public int compareTo(Point o){ return Integer.compare(o.height, height); }
+    public String toString(){ return "("+height+","+index+")"; }
 }
