ab01c7d22538977c7c0831432d9bf7f071198f8f
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-07-06 18:08:11 -0700

    Update minimum-window-substring.java

--- minimum-window-substring.java
+++ minimum-window-substring.java
@@ -2,6 +2,7 @@
 class Solution {
     // Using 2 HashMaps
     // Time: O(n), Space: O(n)
+    // go one by one of begining and ending
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> sMap = new HashMap<Character, Integer>();
         HashMap<Character, Integer> tMap = new HashMap<Character, Integer>();
@@ -31,6 +32,38 @@ class Solution {
         }
         return s.substring(maxB,maxE);
     }
+    
+    // Using 1 HashMap
+    // Time: O(n), Space: O(n)
+    // go ending first, then go begining
+    public String minWindow2(String s, String t) {
+        HashMap<Character, Integer> tMap = new HashMap<Character, Integer>();
+        for(char c : t.toCharArray()) add(tMap, c);
+        int b = 0, e = 0, length = s.length()+1, maxB = 0, maxE = 0, cnt = 0;
+        while(e<s.length()){
+            //System.out.println(b+" "+e+" "+maxB+" "+maxE+" "+cnt+" "+tMap.toString());
+            if(tMap.containsKey(s.charAt(e))){
+                sub(tMap, s.charAt(e));
+                if(tMap.get(s.charAt(e))>=0) cnt++;
+                while(cnt==t.length()){
+                    if((e-b+1)<length){
+                        maxB = b;
+                        maxE = e;
+                        length = (e-b+1);
+                    }
+                    if(tMap.containsKey(s.charAt(b))){
+                        add(tMap, s.charAt(b));
+                        if(tMap.get(s.charAt(b))>0) cnt--;
+                    }
+                    b++;
+                }                
+            }
+            e++;
+            //System.out.println(" "+b+" "+e+" "+maxB+" "+maxE+" "+cnt+" "+tMap.toString());
+        }
+        return length == s.length()+1 ? "" : s.substring(maxB,maxE+1);
+    }
+    
     private void add(Map<Character, Integer> map, char c){ map.put(c, map.getOrDefault(c,0)+1); }
     private void sub(Map<Character, Integer> map, char c){ map.put(c, map.get(c)-1); }
     private boolean isSatisfied(Map<Character, Integer> sMap, Map<Character, Integer> tMap){
