ad10245c5af3fe1f4f0ea0215898415cfec2da8c
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-07-19 16:19:14 -0700

    Update burst-balloons.java

--- burst-balloons.java
+++ burst-balloons.java
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/burst-balloons/description/
-// Divide & Conquer + memo
-// Time: O(n^2), Space: O(n^2)
+
 class Solution {
+    // Divide & Conquer + memo
+    // Time: O(n^2), Space: O(n^2)
     int[] nums2;
     int[][] memo;
     public int maxCoins(int[] nums) {
@@ -23,5 +24,27 @@ class Solution {
         }
         memo[l][r] = ans;
         return ans;
+    }
+    // Dynamic Programming
+    // Time: O(n^3), Space: O(n^2)
+    public int maxCoins2(int[] nums) {
+        int len = nums.length;
+        if(len==0) return 0;
+        
+        int[][] dp = new int[len+2][len+2];
+        for(int range=2; range<(len+2); range++){
+            for(int left=0; left<(len+2-range); left++){
+                int leftNum = 1;
+                if(left>0) leftNum = nums[left-1];
+                int right = left + range;
+                int rightNum = 1;
+                if(right<(len+1)) rightNum = nums[right-1];
+                for(int mid=left+1; mid<right; mid++){
+                    int midNum = nums[mid-1];
+                    dp[left][right] = Math.max(dp[left][right], leftNum*midNum*rightNum + dp[left][mid] + dp[mid][right]);
+                }
+            }
+        }
+        return dp[0][len+1];
     }
 }
