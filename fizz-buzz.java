903c7e21a95a6e7eb79d2e6b14e3fc3e4d6cd9cc
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-07-25 16:22:00 -0700

    Create fizz-buzz.java

---
+++ fizz-buzz.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/fizz-buzz/description/
+// Time: O(n), Space: O(n)
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> r = new ArrayList<String>();
+        for(int i=1; i<=n; i++){
+            StringBuilder sb = new StringBuilder();
+            if(i%3==0) sb.append("Fizz");
+            if(i%5==0) sb.append("Buzz");
+            if(i%3!=0 && i%5!=0) sb.append(Integer.toString(i));
+            r.add(sb.toString());
+        }
+        return r;
+    }
+}
