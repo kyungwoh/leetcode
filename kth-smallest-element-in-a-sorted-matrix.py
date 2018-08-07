076f25dc5f54e8f184e4b00d05ba1e2d7eb8eebe
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-08-06 17:45:31 -0700

    Update kth-smallest-element-in-a-sorted-matrix.py

--- kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix.py
@@ -1,13 +1,25 @@
 # https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
-# Use HeapQ
-# Time: O(klogn), Space: O(n)
 class Solution:
+    # Use Binary search (minNum to maxNum) & (find number with i=0->n-1 & j=n-1->0)
+    # Count numbers by row & column
+    # n = # of matrix, m = range(minNum to maxNum of matrix)
+    # Time: O(nlogm), Space: O(1)
     def kthSmallest(self, matrix, k):
-        """
-        :type matrix: List[List[int]]
-        :type k: int
-        :rtype: int
-        """
+        n = len(matrix)
+        lo,hi = matrix[0][0],matrix[n-1][n-1]
+        while lo<hi:
+            mid,cnt,i,j = (lo+hi)//2,0,0,n-1
+            while i<n:
+                while j>=0 and matrix[i][j]>mid: j -= 1
+                cnt += (j+1)
+                i += 1
+            if k > cnt: lo = mid+1
+            else: hi = mid
+        return lo
+    
+    # Use HeapQ
+    # Time: O(klogn), Space: O(n)
+    def kthSmallest2(self, matrix, k):
         n = len(matrix)
         heap = []
         for i in range(n):
