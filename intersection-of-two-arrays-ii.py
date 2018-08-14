fd24aa78de025618b11f7d557821171b542119dc
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-08-14 14:59:14 -0700

    Update intersection-of-two-arrays-ii.py

--- intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii.py
@@ -33,3 +33,18 @@ class Solution(object):
                 m = min(v, dic2[k])
                 for i in range(m): ans.append(k)
         return ans
+
+    # Sort & merge
+    # Time: O(nlogn), Space: O(1) (without output)
+    def intersect(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        i1, i2, ans = 0, 0, []
+        while i1<len(nums1) and i2<len(nums2):
+            if nums1[i1]==nums2[i2]:
+                ans.append(nums1[i1])
+                i1 += 1
+                i2 += 1
+            elif nums1[i1]<nums2[i2]: i1 += 1
+            else: i2 += 1
+        return ans
