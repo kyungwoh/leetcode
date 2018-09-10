3f52ada5e814a99557abfb634c488fedc104cb50
Kyungwoo Hyun <32881438+kyungwoh@users.noreply.github.com>
2018-09-09 20:28:20 -0700

    Update longest-palindromic-substring.py

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -19,3 +19,19 @@ class Solution:
             if r-l+1>len(ans):
                 ans = s[l:r+1]
         return ans
+    
+    def longestPalindrome2(self, s):
+        def longestStr(l,r):
+            while l>0 and r<len(s)-1 and s[l-1]==s[r+1]:
+                l -= 1
+                r += 1
+            if r-l+1>self.ansLen:
+                self.ansL = l
+                self.ansR = r
+                self.ansLen = r-l+1
+            
+        self.ansL,self.ansR,self.ansLen=0,0,0
+        for i in range(len(s)):
+            longestStr(i,i)
+            if i>0: longestStr(i,i-1)
+        return s[self.ansL:self.ansR+1]
